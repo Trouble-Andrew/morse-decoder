d8db61c38b4a3c496aa6d69ad6ed75b30059b194
Trouble-Andrew <satyr663@gmail.com>
2022-01-29 10:58:02 +0300

    feat: add morse-decoder function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,56 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const chunk = (arr, chunkSize = 1, cache = []) => {
+    const tmp = [...arr];
+    if (chunkSize <= 0) return cache;
+    while (tmp.length) cache.push(tmp.splice(0, chunkSize));
+    return cache;
+}
+
 function decode(expr) {
-    // write your solution here
+    const exprArray = (chunk(expr.split(''), 10));
+    const lettersArray = [];
+    let arrayWithoutEmptySigns;
+    let morseSignArray = [];
+    let result = '';
+
+    for (let letter of exprArray) {
+        let letterArray = chunk(letter, 2);
+        let resultArray = []
+
+        for (const letter of letterArray) {
+            resultArray.push(letter.join(''));
+        }
+        
+        lettersArray.push(resultArray);
+    }    
+
+    arrayWithoutEmptySigns = lettersArray.map(sign => {
+        return sign.filter(sign => sign !== '00');
+    })
+
+    arrayWithoutEmptySigns.forEach(sign => {
+        sign = sign.map(sign => {
+                if (sign === '10') {
+                    return '.';
+                } else if (sign === '11') {
+                    return '-';
+                }
+            }).join('')
+        morseSignArray.push(sign);
+    });
+
+    for (const code of morseSignArray) {
+        console.log(MORSE_TABLE[code]);
+        if (MORSE_TABLE[code] === undefined) {
+            result += ' ';
+        } else {
+            result += MORSE_TABLE[code];
+        }        
+    }
+
+    return result;
 }
 
 module.exports = {
